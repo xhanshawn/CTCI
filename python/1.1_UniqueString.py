81ee3ee38805218a98cc2dc02a20b854194f1737
xhanshawn <xhan@wpi.edu>
2015-09-25 21:10:00 -0400

    initial commit for the local stuffs

---
+++ python/1.1_UniqueString.py
@@ -0,0 +1,16 @@
+# verify if a string has all unique characters.
+def isUnique1(s):
+	return s and len(s) <=128 and len(s) == len(set(list(s)))
+def isUnique2(s):
+	if(not s or len(s) > 128): return False
+	has = set()
+	for c in s:
+		if(c in has): return False
+		else: has.add(c)
+	return True
+slong = ""
+for i in xrange(128): slong += str(chr(i))
+print isUnique1(slong)
+print isUnique2(slong)
+
+
